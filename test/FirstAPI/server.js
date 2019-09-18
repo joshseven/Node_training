c9d9889a09c90d6cb260fac56acbea37836790b7
joshseven <neves.joshua@gmail.com>
2019-09-18 11:36:49 -0400

    get request

--- test/FirstAPI/server.js
+++ test/FirstAPI/server.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParer = require('body-parser');
+
+app.use(bodyParer.json());
+app.use(body.bodyParer.urlencoded({extended: false}));
+
+var ingredients = [
+    {
+        "id": "234kak",
+        "text":"eggs",
+    },
+    {
+        "id": "234kaJ",
+        "text":"Milk",
+    },
+    {
+        "id": "234kDA",
+        "text":"Break",
+    },
+    {
+        "id": "23434k",
+        "text":"Chicken",
+    }
+];
 
 app.get('/', function(request, response){
-    response.send('My first API!');
+    response.send(ingredients);
 });
 
 app.listen(3000, function(){
